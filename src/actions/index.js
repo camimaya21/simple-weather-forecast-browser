290f3764730845e8028f8bf43e0b9bc484db4088
camimaya21 <camilamaya21@gmail.com>
2018-03-27 16:33:53 +0200

    weather api call is working as well as the axios promises middleware

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,18 @@
 import { API_KEY } from 'babel-dotenv';
+import axios from 'axios';
 
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=b6907d289e10d714a6e88b30761fae22${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url); //axios returns promise
+
+  console.log('Request', request);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request //AJAX request, whitch payload is a promise
   };
 }
